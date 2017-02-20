c29d18c7a39a13cd4c5fb1633e4a2d2e5f9206cf
Ichii <danbooru@packer-debian-8-amd64.droplet.local>
2017-02-20 03:46:46 +0000

    remove secondary deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :user, "danbooru"
 set :rails_env, "production"
 server "friendz.japari.me", :roles => %w(web app db), :primary => true, :user => "danbooru"
-server "friends.japari.me", :roles => %w(web app), :user => "danbooru"
+# server "friends.japari.me", :roles => %w(web app), :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
